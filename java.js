74b824f57e67587386fb294dbb6045db7d6dbd0c
Amauri <brownamauri@icloud.com>
2021-07-01 21:28:51 -0400

    new

---
+++ java.js
@@ -0,0 +1,26 @@
+//alert ("Welcome to Amauri's Final Project Website");
+const sounds = ["Shuffle", "Machine"];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+  
+    btn.innerText = sound;
+  
+    btn.addEventListener("click", () => {
+      stopSongs();
+  
+      document.getElementById(sound).play();
+    });
+  
+    document.getElementById("buttons").appendChild(btn);
+  });
+  
+  function stopSongs() {
+    sounds.forEach((sound) => {
+      const song = document.getElementById(sound);
+  
+      song.pause();
+      song.currentTime = 0;
+    });
+  }
